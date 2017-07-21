db2600fdfec46173cb6ac6e82e7fa7375452118f
Jesse Bryan <jesse.bryan22@gmail.com>
2017-07-21 10:29:10 -0700

    Finish specifications 3 and 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,35 @@
 //BUSINESS LOGIC
 
-function Pizza(size, toppings) {
+function Pizza(size, toppings, premium) {
   this.size = size;
   this.toppings = toppings;
+  this.premium = premium;
 }
 
 Pizza.prototype.getPrice = function(){
+  var total = 12;
 
+  switch(this.size) {
+    case "medium":
+      total+= 2.5;
+      break;
+    case "large":
+      total+= 5;
+      break;
+    case "xlarge":
+      total+= 7.5;
+      break;
+  }
+
+  if (this.toppings) {
+    total += (0.75 * this.toppings.length)
+  }
+
+  if (this.toppings) {
+    total += (1.5 * this.premium.length)
+  }
+
+  return total;
 }
 
 
@@ -20,8 +43,14 @@ $(function(){
     $("input:checkbox[name=toppings]:checked").each(function(){
       toppings.push($(this).val());
     });
+    var premiumToppings = [];
+    $("input:checkbox[name=premium-toppings]:checked").each(function(){
+      premiumToppings.push($(this).val());
+    });
 
-    var newPizza = new Pizza(size, toppings);
+
+    var newPizza = new Pizza(size, toppings, premiumToppings);
     console.log(newPizza);
+    console.log(newPizza.getPrice());
   });
 });
